bc6becb2a7da672a6aa8ffdc51b44f690cb66e60
Alexis Delaque <alex.delvaque@gmail.com>
2014-12-17 00:28:06 +0100

    Comparing scroll and element position
    Scroll position to detect target element

--- UltimumOpus/src/views/menu/menu_view.js
+++ UltimumOpus/src/views/menu/menu_view.js
@@ -17,7 +17,29 @@ define([
             //$('.draggable').draggable();
             setTimeout(function(){
                 self.panel(function(){
-                    
+                    var pos = this.positions;
+                    var win = $(window);
+                    win.scroll(function(){
+                        if(win.scrollTop() > pos[1].position){
+                            console.log(pos[0].element)
+                        }else if(win.scrollTop() > pos[2].position && win.scrollTop() < pos[0].position){
+                            console.log(pos[1].element)
+                        }else if(win.scrollTop() > pos[3].position && win.scrollTop() < pos[1].position){
+                            console.log(pos[2].element)
+                        }else if(win.scrollTop() > pos[4].position && win.scrollTop() < pos[2].position){
+                            console.log(pos[3].element)
+                        }else if(win.scrollTop() > pos[5].position && win.scrollTop() < pos[3].position){
+                            console.log(pos[4].element)
+                        }else if(win.scrollTop() > pos[6].position && win.scrollTop() < pos[4].position){
+                            console.log(pos[5].element)
+                        }else if(win.scrollTop() > pos[7].position && win.scrollTop() < pos[5].position){
+                            console.log(pos[6].element)
+                        }else if(win.scrollTop() > pos[8].position && win.scrollTop() < pos[6].position){
+                            console.log(pos[7].element)
+                        }else if(win.scrollTop() < pos[7].position){
+                            console.log(pos[7].element)
+                        };
+                    });
                 })
             },3500);
            
