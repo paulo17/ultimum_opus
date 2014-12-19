f1168828fe5cfde9c03c77988c84166934176af6
maximeberthelot <hello@maximeberthelot.fr>
2014-12-19 06:39:57 +0100

    MAJ About & Template sidebar

--- UltimumOpus/src/views/myStory/myStory_view.js
+++ UltimumOpus/src/views/myStory/myStory_view.js
@@ -24,22 +24,36 @@ define([
         /**
         *    Stop the parallax animation at the end of the page
         **/
-        animate: function(){
-            console.log('ee');
-        },
 
         myEndAnim: function(){
             if(window.percentDone<0.05) {
                 TweenMax.to($("#paralax"), .85, {"opacity": '0', ease: Expo.easeInOut});
 
-                $("#end_1").fadeIn(3000, function () {
-                    $("#end_2").fadeIn(3000)
+                $("#end_2").fadeIn(3000, function () {
+                    $("#end_1").fadeIn(3000, function(){
+                        //Set TextShadow on Opus
+                        $.fx.step.textShadowBlur = function(fx) {
+                            $(fx.elem)
+                                .prop('textShadowBlur', fx.now)
+                                .css({textShadow: '0 0 ' + Math.floor(fx.now) + 'px white'});
+                        };
+
+                        setInterval(function() {
+                            $(".endctnt").animate({textShadowBlur:20}, {duration: 1500, complete: function() {
+                                $(".endctnt").animate({textShadowBlur:1}, {duration: 2500});
+                            }});
+                        }, 1000);
+                    })
                 })
             }
 
             if(window.percentDone>0.05){
                 TweenMax.to($("#paralax"), .85, { "opacity": '.2', ease: Expo.easeInOut });
             }
+
+            // Make shine on title
+
+
         },
 
         render: function(){
